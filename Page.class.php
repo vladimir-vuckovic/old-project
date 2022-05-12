95a3b33211aabdf526f01952be62e2572be3e367
Vladimir Vuckovic <mirvuc@gmail.com>
2022-05-12 14:32:51 +0200

    changed

--- Page.class.php
+++ Page.class.php
@@ -1,7 +1,7 @@
 <?php
 class Page extends Entity
 {
-	public static $table = "sr_pages_SSSSS";
+	public static $table = "sr_pages_SSSSS-HHHH";
 	public static $columns = array("name","modules","template","menu","footer");
 	public static $id_column = "id";
 	public $id,$name,$modules,$template;
